6c61d61ee23183db4978b06041937ccdab38cc85
XtraCube <72575280+XtraCube@users.noreply.github.com>
2025-07-08 18:45:35 -0400

    split mods

---
+++ AuthFix/AuthPlugin.cs
@@ -0,0 +1,71 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using Epic.OnlineServices;
+using Epic.OnlineServices.Connect;
+using HarmonyLib;
+using Il2CppSystem;
+
+namespace AuthFix;
+
+[BepInAutoPlugin("dev.xtracube.authplugin")]
+// ReSharper disable once ClassNeverInstantiated.Global
+public partial class AuthPlugin : BasePlugin
+{
+    public override void Load()
+    {
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+    }
+
+    [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.LoginWithCorrectPlatformImpl))]
+    public static class AuthPatch
+    {
+        // ReSharper disable once InconsistentNaming
+        public static bool Prefix(EOSManager __instance, OnLoginCallback successCallbackIn)
+        {
+            var loginOptions = new LoginOptions();
+            var credentials = new Credentials();
+            credentials.Token = new Utf8String("DUMMY");
+            credentials.Type = ExternalCredentialType.ItchioKey;
+            loginOptions.Credentials = new Nullable<Credentials>(credentials);
+            var loginOptions2 = loginOptions;
+            __instance.PlatformInterface.GetConnectInterface().Login(ref loginOptions2, null, successCallbackIn);
+            __instance.stopTimeOutCheck = true;
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(EOSManager), nameof(EOSManager.EOSConnectPlatformLoginCallback))]
+    public static class AuthCallbackPatch
+    {
+        // ReSharper disable once InconsistentNaming
+        public static bool Prefix(EOSManager __instance, LoginCallbackInfo loginCallbackInfo)
+        {
+            if (loginCallbackInfo.ResultCode == Result.Success)
+            {
+                return true;
+            }
+
+            __instance.ContinueInOfflineMode();
+            return false;
+        }
+    }
+    
+    [HarmonyPatch(typeof(StoreManager), nameof(StoreManager.InitiateStorePurchaseStar))]
+    public static class DisableStarBuyPatch
+    {
+        // ReSharper disable once InconsistentNaming
+        public static bool Prefix()
+        {
+            var purchasePopUp = StoreMenu.Instance.plsWaitModal;
+            purchasePopUp.waitingText.gameObject.SetActive(false);
+            purchasePopUp.titleText.text = "NOT SUPPORTED";
+            purchasePopUp.infoText.text = "Platform Purchases are not supported in Starlight.\nBuy in the vanilla client instead.";
+            purchasePopUp.infoText.gameObject.SetActive(true);
+            purchasePopUp.controllerFocusHolder.gameObject.SetActive(true);
+            purchasePopUp.closeButton.gameObject.SetActive(true);
+            return false;
+        }
+    }
+}
---
+++ LightingPlus/LightingPlugin.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LightingPlus;
+
+[BepInAutoPlugin("dev.xtracube.lightingplugin")]
+public partial class LightingPlugin : BasePlugin
+{
+    private static LightingPlugin Instance { get; set; }
+    
+    private ConfigEntry<int> TargetFrameRate { get; set; }
+    private ConfigEntry<LightSourceRendererType> LightSourceRenderMode { get; set; }
+    
+    public LightingPlugin()
+    {
+        Instance = this;
+        TargetFrameRate = Config.Bind("General", "Target Frame Rate", 60, "The target frame rate of the game");
+        LightSourceRenderMode = Config.Bind("General", "Light Source Mode", LightSourceRendererType.GPU, "The renderer type of the light source");
+    }
+
+    public override void Load()
+    {
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+    }
+
+    // frame rate patch
+    [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.Awake))]
+    public static class AmongUsClientPatch
+    {
+        public static void Postfix()
+        { 
+            Application.targetFrameRate = Instance.TargetFrameRate.Value;
+        }
+    }
+
+    // light source patch
+    [HarmonyPatch(typeof(LightSource), nameof(LightSource.Initialize))]
+    public static class LightSourcePatch
+    {
+        // ReSharper disable once InconsistentNaming
+        public static void Prefix(LightSource __instance)
+        {
+            __instance.rendererType = Instance.LightSourceRenderMode.Value;
+        }
+    }
+}
