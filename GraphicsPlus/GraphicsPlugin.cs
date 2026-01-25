a5109ec46b30dd4e60d94b1f4c39e1dc6cd7018f
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-01-25 12:51:50 -0500

    add resolution options

--- GraphicsPlus/GraphicsPlugin.cs
+++ GraphicsPlus/GraphicsPlugin.cs
@@ -13,27 +13,38 @@ public partial class GraphicsPlugin : BasePlugin
     private static GraphicsPlugin Instance { get; set; }
     
     private ConfigEntry<int> TargetFrameRate { get; set; }
-    private ConfigEntry<LightSourceRendererType> LightSourceRenderMode { get; set; }
+
+    private ConfigEntry<bool> FullResolution { get; set; }
     
     public GraphicsPlugin()
     {
         Instance = this;
         TargetFrameRate = Config.Bind("General", "Target Frame Rate", 60, "The target frame rate of the game");
-        LightSourceRenderMode = Config.Bind("General", "Light Source Mode", LightSourceRendererType.GPU, "The renderer type of the light source");
+        FullResolution = Config.Bind("General", "Increase Resolution", false, "Set the game to use the display resolution.");
     }
 
     public override void Load()
     {
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), Id);
+        Log.LogInfo("GraphicsPlus loaded!");
     }
 
-    // frame rate patch
     [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.Awake))]
-    public static class AmongUsClientPatch
+    public static class FrameRatePatch
     {
         public static void Postfix()
         { 
+            Instance.Log.LogInfo($"Setting target frame rate to {Instance.TargetFrameRate.Value}");
             Application.targetFrameRate = Instance.TargetFrameRate.Value;
+
+            if (!Instance.FullResolution.Value) return;
+
+            Instance.Log.LogInfo($"Setting fullscreen resolution to {Display.main.systemWidth}x{Display.main.systemHeight}");
+            Screen.SetResolution(
+                Display.main.systemWidth,
+                Display.main.systemHeight,
+                true
+            );
         }
     }
 }
